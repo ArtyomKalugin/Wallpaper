82678ee1f014a8d07d12f6e71841a16fe48576e2
Artem <Artem>
2021-12-24 15:17:19 +0300

    Initial Commit

---
+++ Wallpaper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wallpaper
+//
+//  Created by Artem Kalugin on 24.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
