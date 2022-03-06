cf4ad19602af7fa315a1bdbdc4731c3a56e4df8c
Artem <Artem>
2022-03-06 22:38:04 +0300

    added an annual subscription

--- Wallpaper/Presentation /Purchase Presentation /PurchaseViewController.swift
+++ Wallpaper/Presentation /Purchase Presentation /PurchaseViewController.swift
@@ -12,15 +12,32 @@ class PurchaseViewController: UIViewController {
 
     // Outlet properties
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var annualContainerView: UIView!
     @IBOutlet weak var buyButton: UIButton!
     @IBOutlet weak var restoreButton: UIButton!
     @IBOutlet weak var privacyTextView: UITextView!
+    
+    //Private properties
+    private var condition = 0
     
     // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
         configure()
+    }
+    
+    // Objc functions
+    @objc func handleLeftTapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+        condition = 0
+        annualContainerView.layer.borderColor = #colorLiteral(red: 0.2816390693, green: 0.3066759408, blue: 0.3362169862, alpha: 1)
+        containerView.layer.borderColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
+    }
+    
+    @objc func handleRightTapGesture(_ gestureRecognizer: UITapGestureRecognizer) {
+        condition = 1
+        containerView.layer.borderColor = #colorLiteral(red: 0.2816390693, green: 0.3066759408, blue: 0.3362169862, alpha: 1)
+        annualContainerView.layer.borderColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
     }
 
     // MARK: - Button actions
@@ -40,11 +57,24 @@ class PurchaseViewController: UIViewController {
 
     // Private functions
     private func configure() {
+        let leftRecognizer = UITapGestureRecognizer()
+        leftRecognizer.addTarget(self, action: #selector(handleLeftTapGesture(_:)))
+        containerView.addGestureRecognizer(leftRecognizer)
+        
+        let rightRecognizer = UITapGestureRecognizer()
+        rightRecognizer.addTarget(self, action: #selector(handleRightTapGesture(_:)))
+        annualContainerView.addGestureRecognizer(rightRecognizer)
+        
         containerView.layer.cornerRadius = 15
         containerView.clipsToBounds = true
         containerView.layer.borderWidth = 3.0
         containerView.layer.borderColor = #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)
-
+        
+        annualContainerView.layer.cornerRadius = 15
+        annualContainerView.clipsToBounds = true
+        annualContainerView.layer.borderWidth = 3.0
+        annualContainerView.layer.borderColor = #colorLiteral(red: 0.2816390693, green: 0.3066759408, blue: 0.3362169862, alpha: 1)
+        
         buyButton.layer.cornerRadius = 10
         buyButton.clipsToBounds = true
 
@@ -75,16 +105,25 @@ class PurchaseViewController: UIViewController {
     }
 
     private func fetchPackages(completion: @escaping (Purchases.Package) -> Void) {
-        Purchases.shared.offerings { offerings, error in
+        Purchases.shared.offerings { [weak self] offerings, error in
             guard let offerings = offerings, error == nil else {
                 return
             }
-
-            guard let package = offerings.all.first?.value.availablePackages.first else {
-                return
+            
+            if self?.condition == 0 {
+                guard let package = offerings.all.first?.value.availablePackages.first else {
+                    return
+                }
+                
+                completion(package)
+                
+            } else {
+                guard let package = offerings.all.first?.value.availablePackages[1] else {
+                    return
+                }
+                
+                completion(package)
             }
-
-            completion(package)
         }
     }
 
